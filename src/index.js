45a2e0a80b607006f88dd94a502983534c2da9de
@lahiruchathuranga19981214 <lahiruchathuranga19981214@gmail.com>
2023-06-20 22:51:55 +0530

    CHG | Change store

--- src/index.js
+++ src/index.js
@@ -3,22 +3,23 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
-import { ApiProvider } from '@reduxjs/toolkit/dist/query/react';
 import { productsApi } from './features/apiSlice';
+import { setupListeners } from '@reduxjs/toolkit/dist/query';
 
 const store = configureStore({
   reducer: {
+    [productsApi.reducerPath]: productsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(productsApi.middleware),
+});
 
-  }
-})
+setupListeners(store.dispatch);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ApiProvider api={productsApi}>
-        <App />
-      </ApiProvider>
+      <App />
     </Provider>
   </React.StrictMode>
 );
